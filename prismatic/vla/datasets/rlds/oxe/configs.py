aa461702be56d7f8aa4770181c951a42af9836f8
qjtdsqqm94akkyysgjdqo3hx1jn6l17 <18874@student.vgu.edu.vn>
2026-03-01 23:21:56 +0700

    add support for raceVLA repo

--- prismatic/vla/datasets/rlds/oxe/configs.py
+++ prismatic/vla/datasets/rlds/oxe/configs.py
@@ -52,6 +52,13 @@ class ActionEncoding(IntEnum):
 
 # === Individual Dataset Configs ===
 OXE_DATASET_CONFIGS = {
+    "race_vla_data": {
+        "image_obs_keys": {"primary": "image", "secondary": None, "wrist": None},
+        "depth_obs_keys": {"primary": None, "secondary": None, "wrist": None},
+        "state_obs_keys": "state",
+        "state_encoding": StateEncoding.POS_EULER,
+        "action_encoding": ActionEncoding.EEF_POS,
+    },
     "fractal20220817_data": {
         "image_obs_keys": {"primary": "image", "secondary": None, "wrist": None},
         "depth_obs_keys": {"primary": None, "secondary": None, "wrist": None},
--- prismatic/vla/datasets/rlds/oxe/transforms.py
+++ prismatic/vla/datasets/rlds/oxe/transforms.py
@@ -28,6 +28,10 @@ from prismatic.vla.datasets.rlds.utils.data_utils import (
 )
 
 
+def no_dataset_transform(trajectory: Dict[str, Any]) -> Dict[str, Any]:
+    return trajectory
+
+
 def bridge_oxe_dataset_transform(trajectory: Dict[str, Any]) -> Dict[str, Any]:
     """
     Applies to version of Bridge V2 in Open X-Embodiment mixture.
@@ -843,6 +847,7 @@ def libero_dataset_transform(trajectory: Dict[str, Any]) -> Dict[str, Any]:
 
 # === Registry ===
 OXE_STANDARDIZATION_TRANSFORMS = {
+    "race_vla_data": no_dataset_transform,
     "bridge_oxe": bridge_oxe_dataset_transform,
     "bridge_orig": bridge_orig_dataset_transform,
     "bridge_dataset": bridge_orig_dataset_transform,
